32e4a3bae069e400378b61247c3006262e16a821
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-11-25 17:52:00 -0600

    Added hello world and other files

---
+++ brokenJS.js
@@ -0,0 +1,9 @@
+        //author Jeff Shepherd
+        var i = 0;
+        while (true) {
+            console.log(i);
+            i = i + 3;
+            if (i == 18) {
+                break;
+            }
+        }
---
+++ week-1/hello-world.js
@@ -0,0 +1,11 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Professor Krasso
+; Date: 11/25/2019
+; Modified By: Jeff Shepherd
+; Description: Hello World
+;===========================================
+*/ 
+
+console.log("Hello World, my name is Jeff Shepherd");
