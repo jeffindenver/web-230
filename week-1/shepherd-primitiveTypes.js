f08af614446b2a27c8ea8d3d43838268a66bb113
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-11-25 18:33:51 -0600

    adding assignment primitive types

---
+++ week-1/shepherd-primitiveTypes.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Jeff Shepherd
+; Date: 11/25/2019
+; Description: Primitive Types
+;===========================================
+*/ 
+
+var firstName = "Jeff";
+var lastName = "Shepherd";
+var age = 48;
+var street = "3000 Farnam St";
+var city = "Omaha";
+var zipCode = "68104";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
