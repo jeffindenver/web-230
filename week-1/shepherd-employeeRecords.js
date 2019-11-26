97a3b964aa38f0350f0ffb8a5b2a1f12cfc45100
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-11-25 20:44:40 -0600

    Removed unneeded parseDate function and added comments

--- week-1/shepherd-employeeRecords.js
+++ week-1/shepherd-employeeRecords.js
@@ -9,21 +9,17 @@
 ;===========================================
 */
 
+//Employee constructor
 function Employee(fname, lname, address, payRate,
     hireDate) {
     this.fname = fname;
     this.lname = lname;
     this.address = address;
     this.payRate = payRate;
-    this.hireDate = new Date(parseDate(hireDate));
+    this.hireDate = new Date(hireDate);
 }
 
-function parseDate(aDate) {
-    //Date should be formatted as "year month day" with zero-based month
-    theDate = aDate.split(" ", 3);
-    return (theDate[0] + "," + theDate[1] + "," + theDate[2]);
-}
-
+//the toString function returns a string representation of the Employee object
 Employee.prototype.toString = function () {
     var newline = "\n";
     var output = "Name: ".concat(this.fname, " ", this.lname, newline,
@@ -32,9 +28,9 @@ Employee.prototype.toString = function () {
     return output;
 }
 
-var jShepherd = new Employee("Jeff", "Shepherd", "3000 Farnam St", 20.0, "2015 1 9");
-var lJohn = new Employee("Linda", "John", "1900 Main St", 16.0, "2009 2 10");
-var mHamill = new Employee("Mark", "Hamill", "200 Rodeo Dr", 100.0, "2015 10 11");
+var jShepherd = new Employee("Jeff", "Shepherd", "3000 Farnam St", 20.0, "2015, 1, 9");
+var lJohn = new Employee("Linda", "John", "1900 Main St", 16.0, "2009, 2, 10");
+var mHamill = new Employee("Mark", "Hamill", "200 Rodeo Dr", 100.0, "2015, 10, 11");
 
 console.log(jShepherd.toString());
 console.log(lJohn.toString());
