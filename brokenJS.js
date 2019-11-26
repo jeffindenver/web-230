6082de0c5071e8587ed06acff08b50259f8a73fa
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-11-25 18:07:47 -0600

    Removed files

--- brokenJS.js
+++ brokenJS.js
@@ -1,5 +1,14 @@
-        //author Jeff Shepherd
-        var i = 0;
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Jeff Shepherd
+; Date: 11/25/2019
+; Modified By: Jeff Shepherd
+; Description: Broken JavaScript
+;===========================================
+*/ 
+      //Count to ten
+        var i == 0;
         while (true) {
             console.log(i);
             i = i + 3;
