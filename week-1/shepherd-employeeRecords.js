f0dcc4fec4cda0eddd604e44324bfa7c70b22c88
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-11-30 17:08:52 -0600

    Refactored the date output to use the built in function toLocalDateString

--- week-1/shepherd-employeeRecords.js
+++ week-1/shepherd-employeeRecords.js
@@ -24,7 +24,7 @@ Employee.prototype.toString = function () {
     var newline = "\n";
     var output = "Name: ".concat(this.fname, " ", this.lname, newline,
         "Address: ", this.address, newline, "Pay rate: $", this.payRate.toFixed(1), newline,
-        "Hire Date: ", this.hireDate.getMonth() + 1, "/", this.hireDate.getDate(), "/", this.hireDate.getFullYear());
+        "Hire Date: ", this.hireDate.toLocaleDateString());
     return output;
 }
 
