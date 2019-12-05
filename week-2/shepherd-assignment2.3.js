591599954bea82296d492a4589161ba46e59aa93
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-12-04 18:39:13 -0600

    Corrected typo in assignment 2.3

--- week-2/shepherd-assignment2.3.js
+++ week-2/shepherd-assignment2.3.js
@@ -9,7 +9,7 @@
 */
 
 const header = require('../shepherd-header.js');
-console.log(header.display("Jeff", "Shepherd", "Assignment 2.4"));
+console.log(header.display("Jeff", "Shepherd", "Assignment 2.3"));
 
 function myName() {
   return myName.jeff;
