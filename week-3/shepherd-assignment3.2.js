6088f8a52b3cb519fa53cac20d9170b158fb33f0
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-12-09 13:31:48 -0600

    added assignment 3.2

---
+++ week-3/shepherd-assignment3.2.js
@@ -0,0 +1,47 @@
+/*
+============================================
+; Title:  shepherd-assignment3.2.js
+; Author: Jeff Shepherd
+; Date:   9 December 2019
+; Modified By:
+; Description: Write pattern matching
+; functions.
+;===========================================
+*/
+
+/**
+ * Params: any two
+ * Response: true or false
+ * Description: returns the equality of two items
+ */
+function match(item1, item2) {
+  return (item1 === item2);
+}
+
+/**
+ * Params: any two
+ * Response: <item1> and <item2> do not match!
+ * Description: Returns a string, saying items do not match.
+ */
+function logMismatch(item1, item2) {
+  return item1 + " and " + item2 + " do not match!";
+}
+
+/**
+ * Params: any two
+ * Response: <item1> and <item2> match!
+ * Description: Returns a string, saying that items match.
+ */
+function logMatch(item1, item2) {
+  return item1 + " and " + item2 + " match!";
+}
+
+let itemsList = ["Truck", "Car", "Bike", "Bike", "Four", "Three"];
+
+for (let i = 0; i < 6; i+=2) {
+  if (match(itemsList[i], itemsList[i+1])) {
+    console.log(logMatch(itemsList[i], itemsList[i+1]));
+  } else {
+    console.log(logMismatch(itemsList[i], itemsList[i+1]));
+  }
+}
