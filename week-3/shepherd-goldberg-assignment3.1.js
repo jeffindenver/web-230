f82de147c57cc116748670a6a26929b9d08fe4f6
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-12-10 12:07:17 -0600

    added shepherd-goldberg-assignment3.1.js

---
+++ week-3/shepherd-goldberg-assignment3.1.js
@@ -0,0 +1,54 @@
+/*
+============================================
+; Title:  shepherd-washington-assignment3.1.js
+; Author: Mike Goldberg
+; Date:   10 December 2019
+; Modified By: Jeff Shepherd
+; Description: Correction to Mike's code.
+;===========================================
+*/
+
+const header = require("../shepherd-header.js");
+console.log(header.display("Jeff", "Shepherd", "Assignment 3.1"));
+console.log("\n");
+
+/*
+    expected output:
+    " Yo, Ho, Yo, Ho, it's the pirates life for me!"
+*/
+
+// variable declaration
+var yo = " Yo";
+var ho = " Ho";
+
+// Alternate printing the words Yo & Ho 2 times, then sing the song on the 5th rotation
+
+function piratesTheme() {
+    var song = [];
+    for (i = 0; i < 5; i++){ //JS. Added the missing "+" to the increment operator
+        if (isOdd(i)) {
+            song.push(ho);
+        } else if (isEven(i) && i === 4){
+            song.push(" it's the pirate's Life for me!");
+        } else {
+            song.push(yo);
+        }
+    }
+
+    console.log(song.join(","))
+}
+
+//takes a number and divides it by 2. If the remainder is not 0, the number is odd
+function isOdd(number) {
+  return number % 2 !== 0;
+}
+
+//takes a number and divides it by 2. If the remainder is 0, the number is even
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+// output
+piratesTheme(); //JS. Added missing semicolon
+
+//  end program
