66e00cd56ae4aea2a88534ef7d068e02b0f66b3c
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-12-16 13:30:39 -0600

    Added assignment 4.2

---
+++ week-4/shepherd-assignment4.2.js
@@ -0,0 +1,30 @@
+/*
+============================================
+; Title:  shepherd-assignment4.2.js
+; Author: Professor Krasso
+; Date:   16 December 2019
+; Modified By:
+; Description: Create and use an array
+;===========================================
+*/
+
+"use strict";
+
+const header = require('../shepherd-header.js');
+console.log(header.display("Jeff", "Shepherd", "Assignment 4.2"));
+console.log("");
+
+let fruits = ["Apple", "Orange", "Banana", "Mango", "Pear"];
+
+/**
+ * Params: a string array
+ * Response: output to the console
+ * Description: console log the elements of a string array
+ */
+function getFruit(items) {
+  for (let item of items) {
+    console.log(item);
+  }
+}
+
+getFruit(fruits);
