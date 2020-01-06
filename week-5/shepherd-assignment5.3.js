ddd8947f63d3f606641dd8394878877aa3476c41
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-01-06 17:21:50 -0600

    added assignment 5.3

---
+++ week-5/shepherd-assignment5.3.js
@@ -0,0 +1,63 @@
+/*
+============================================
+; Title:  shepherd-assignment5.3.js
+; Author: Jeff Shepherd
+; Date:   6 January 2020
+; Modified By:
+; Description: Create an array of composer
+; objects and iterate over them, printing
+; results.
+;===========================================
+*/
+
+"use strict";
+
+const header = require('../shepherd-header.js');
+console.log(header.display("Jeff", "Shepherd", "Assignment 5.3"));
+console.log("");
+
+let composers = [
+  {
+    firstName: "Ludwig",
+    lastName: "Beethoven",
+    genre: "Classical",
+    rating: 8
+  },
+
+  {
+    firstName: "Wolfgang",
+    lastName: "Mozart",
+    genre: "Classical",
+    rating: 10
+  },
+
+  {
+    firstName: "Johann",
+    lastName: "Bach",
+    genre: "Classical",
+    rating: 9
+  },
+
+  {
+    firstName: "Joseph",
+    lastName: "Haydn",
+    genre: "Classical",
+    rating: 6
+  },
+
+  {
+    firstName: "Franz",
+    lastName: "Schubert",
+    genre: "Classical",
+    rating: 5
+  }
+]
+
+console.log("-- COMPOSERS --");
+composers.forEach(item => printComposer(item));
+
+function printComposer(item) {
+  console.log("Last Name: " + item.lastName +
+    ", Genre: " + item.genre +
+    ", Rating: " + item.rating.toString());
+}
