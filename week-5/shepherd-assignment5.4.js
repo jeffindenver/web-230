4dabc43b8a93f1b53de1cb34bc23a9285882b524
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-01-07 13:12:35 -0600

    Added assignment 5.4

---
+++ week-5/shepherd-assignment5.4.js
@@ -0,0 +1,91 @@
+/*
+============================================
+; Title:  shepherd-assignment5.4.js
+; Author: Jeff Shepherd
+; Date:   6 January 2020
+; Modified By:
+; Description: Create an array of composer
+; objects, reduce properties, and iterate over
+; them, printing results.
+;===========================================
+*/
+
+"use strict";
+
+const header = require('../shepherd-header.js');
+console.log(header.display("Jeff", "Shepherd", "Assignment 5.4"));
+console.log("");
+
+let composers = initializeComposers();
+let wellRatedComposers = composers.map(item => reduceProperties(item, ["lastName", "rating"]));
+let classicalComposers = composers.map(item => reduceProperties(item, ["lastName", "genre"]));
+
+console.log("-- COMPOSER BY RATING --");
+wellRatedComposers.forEach(item => console.log("Rating: " + item.rating + "\n" +
+  "Composer: " + item.lastName + "\n"));
+
+console.log("-- COMPOSER BY GENRE --");
+classicalComposers.forEach(item => console.log("Genre: " + item.genre + "\n" +
+  "Composer: " + item.lastName + "\n"));
+
+/**
+ * Params: an Object and an arg array
+ * Response: returns a new object
+ * Description: sets the properties of
+ * of an object. Can return undefined
+ * if none of the arguments match
+ * properties of the passed object.
+ */
+function reduceProperties(anObject, args) {
+  let newObject = {};
+  for (let arg of args) {
+    if(anObject.hasOwnProperty(arg)) {
+      newObject[arg] = anObject[arg];
+    }
+  }
+  return newObject;
+}
+
+/**
+ * Params: none
+ * Response: returns an array of objects
+ * Description: sets the properties of
+ * several Composer objects.
+ */
+function initializeComposers() {
+  return [{
+      firstName: "Ludwig",
+      lastName: "Beethoven",
+      genre: "Classical",
+      rating: 8
+    },
+
+    {
+      firstName: "Wolfgang",
+      lastName: "Mozart",
+      genre: "Classical",
+      rating: 10
+    },
+
+    {
+      firstName: "Johann",
+      lastName: "Bach",
+      genre: "Classical",
+      rating: 9
+    },
+
+    {
+      firstName: "Joseph",
+      lastName: "Haydn",
+      genre: "Classical",
+      rating: 6
+    },
+
+    {
+      firstName: "Franz",
+      lastName: "Schubert",
+      genre: "Classical",
+      rating: 5
+    }
+  ];
+}
