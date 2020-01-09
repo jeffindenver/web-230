e4cb576c34be896b02052b37a0e281cd578c49ce
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-01-09 16:54:03 -0600

    Added response to pierce assignment5.1

---
+++ week-5/pierce-shepherd-assignment5.1.js
@@ -0,0 +1,35 @@
+/*
+============================================
+; Title: Discussion 5.1 Advanced Arrays
+; Author: Kimberly Pierce
+; Date:   7 January 2020
+; Modified By: Jeff Shepherd
+; Description: Keyed Collection Arrays with Errors
+;===========================================
+*/
+
+
+var myMap = new Map();//create new map
+//JS added the "new" keyword
+
+
+//set key value pairs
+
+
+myMap.set ('bigDog', 'Swidgen');
+
+
+myMap.set ('littleDog', 'Molly'); //JS separated the pair with a comma
+
+
+myMap.set ('blackCat', 'Wee');
+
+
+myMap.set ('otherCat', 'Miss Kitty');
+
+
+
+//call value of one pair
+
+
+console.log(myMap.get('bigDog'));
